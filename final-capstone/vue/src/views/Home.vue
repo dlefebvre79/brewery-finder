f1eac3a2976b6b58c2aacfc8e76e18bb7db86bc5
Reconna <Taylorrec1991@gmail.com>
2021-03-08 13:33:18 -0500

    added files

--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -1,12 +1,14 @@
 
 <template>
-<div id ="container">
-
-  <div class="home">
-    <h1>WELCOME</h1>
-    <img src ="https://cdn.craftbeer.com/wp-content/uploads/Solera_Brewing.jpg"  width="1800" height="1000"/>
-    <p>You must be authenticated to see this</p>
-  </div>
+<div class ="container">
+  <div class="nav">Nav</div>
+    <main>Main</main>
+    <div id ="sidebar">Sidebar</div>
+    <div id ="content1">Content1</div>
+    <div id ="content2">Content2</div>
+    <div id = "content3">Content3</div>
+    <footer>Footer</footer>
+    <p>Welcome</p>
   </div>
 </template>
 
@@ -16,12 +18,46 @@ export default {
 };
 </script>
 <style>
-.home{
-  text-align: center;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-  color: gold ;
+.container{
+  display:grid;
+  height:100vh;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 0.2fr 1.5fr 1.2fr 0.8fr;
+  grid-template-areas:
+   "nav nav nav nav"
+   "sidebar main main main"
+   "sidebar content1 content2 content 3"
+   "sidebar footer footer footer";
 }
-img{
-  border-radius: 8px;
+.nav{
+  background-color: coral;
+  grid-area:nav;
 }
+main{
+  background-color: cornflowerblue;
+  grid-area:main;
+}
+#sidebar{
+  background-color: crimson;
+  grid-area:sidebar;
+}
+
+#content1{
+background-color: darkmagenta;
+grid-area: content1;
+}
+#content2{
+  background-color: darkolivegreen;
+  grid-area: content2;
+}
+#content3{
+  background-color: orange;
+  grid-area: content3;
+}
+footer{
+  background-color: orchid;
+  grid-area: footer;
+}
+
+
 </style>
