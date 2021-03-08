bf8d2e4da7174354d29c72098a9a653eccc4d209
Reconna <Taylorrec1991@gmail.com>
2021-03-08 15:11:22 -0500

    added files

--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -1,7 +1,14 @@
+
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+<div class ="container">
+  <div class="nav">Nav</div>
+    <main>Main</main>
+    <div id ="sidebar">Sidebar</div>
+    <div id ="content1">Content1</div>
+    <div id ="content2">Content2</div>
+    <div id = "content3">Content3</div>
+    <footer>Footer</footer>
+    <p>Welcome</p>
   </div>
 </template>
 
@@ -10,3 +17,47 @@ export default {
   name: "home"
 };
 </script>
+<style>
+.container{
+  display:grid;
+  height:100vh;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 0.2fr 1.5fr 1.2fr 0.8fr;
+  grid-template-areas:
+   "nav nav nav nav"
+   "sidebar main main main"
+   "sidebar content1 content2 content 3"
+   "sidebar footer footer footer";
+}
+.nav{
+  background-color: coral;
+  grid-area:nav;
+}
+main{
+  background-color: cornflowerblue;
+  grid-area:main;
+}
+#sidebar{
+  background-color: crimson;
+  grid-area:sidebar;
+}
+
+#content1{
+background-color: darkmagenta;
+grid-area: content1;
+}
+#content2{
+  background-color: darkolivegreen;
+  grid-area: content2;
+}
+#content3{
+  background-color: orange;
+  grid-area: content3;
+}
+footer{
+  background-color: orchid;
+  grid-area: footer;
+}
+
+
+</style>
