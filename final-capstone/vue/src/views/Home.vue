726366a6c5185368920be9a056901d4193269644
Reconna <Taylorrec1991@gmail.com>
2021-03-08 08:59:28 -0500

    new files

--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -1,7 +1,12 @@
+
 <template>
+<div id ="container">
+
   <div class="home">
-    <h1>Home</h1>
+    <h1>WELCOME</h1>
+    <img src ="https://cdn.craftbeer.com/wp-content/uploads/Solera_Brewing.jpg"  width="1800" height="1000"/>
     <p>You must be authenticated to see this</p>
+  </div>
   </div>
 </template>
 
@@ -10,3 +15,13 @@ export default {
   name: "home"
 };
 </script>
+<style>
+.home{
+  text-align: center;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color: gold ;
+}
+img{
+  border-radius: 8px;
+}
+</style>
