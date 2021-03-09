d5beb53a4551fb3d18479ee8136c1d68141db420
Ransom Ward <Reward8604@gmail.com>
2021-03-09 11:57:29 -0500

    insert seed data.sql update

--- final-capstone/java/database/04_insert_seed_data.sql
+++ final-capstone/java/database/04_insert_seed_data.sql
@@ -30,12 +30,52 @@ VALUES (2, 'Saucy Brew Works', '2885 Detroit Rd', 'Cleveland', 44113,'(216) 666-
          Beer fanatics. Always up for adventure. They decided to transform the Steelman Building into the kind of place that delivers nothing less than crazy good beer and legendary good times.',
         'Two-tiered brewery offering housemade European-style beer & customizable thin-crust pizzas.', 'true', 'true', 'true', 'true', 'https://www.saucybrewworks.com/',
         'https://www.google.com/maps/place/Saucy+Brew+Works/@41.489633,-81.7128136,17z/data=!3m1!4b1!4m5!3m4!1s0x8830f06856cfcaf7:0xb7cc96529c4594ea!8m2!3d41.489629!4d-81.7106249', 2);
+--insert John        
+INSERT INTO breweries (brewery_id, name, address, city, zipcode, phone_number, days_of_operation, business_hours, history_desc, atmosphere, is_family_friendly, is_patio, is_food, is_active, website_url, google_maps_url, user_id)
+VALUES (3, 'Rocky River Brewing Company'
+	, '21290 Center Ridge Road'
+	, 'Rocky River'
+	, '44116'
+	, '440-895-2739'
+	, '7 Days'
+	, '11:30 a.m. - 10:00 p.m. F/S 11:00 p.m.'
+	, 'Pleasing Palettes Since 1998 - AWARD-WINNING HANDCRAFTED FOOD & BEER - BrewPub & Restaurant.'
+	, 'Classic Bar and Restaurant'
+	, 'true'
+	, 'true'
+	, 'true'
+	, 'true'
+	, 'https://www.rockyriverbrewco.com/'
+	, 'https://www.google.com/maps/place/21290+Center+Ridge+Rd,+Rocky+River,+OH+44116/@41.4610007,-81.8583192,17z/data=!3m1!4b1!4m5!3m4!1s0x8830ed4f8cc1d2c9:0xa3b50ec4ba97919a!8m2!3d41.4610007!4d-81.8561305'
+	, 2
+	);
+	
+INSERT INTO breweries (brewery_id, name, address, city, zipcode, phone_number, days_of_operation, business_hours, history_desc, atmosphere, is_family_friendly, is_patio, is_food, is_active, website_url, google_maps_url, user_id)
+VALUES (4, 'Sibling Revelry Brewing'
+	, '29305 Clemens Rd'
+	, 'Westlake'
+	, '44145'
+	, '440-471-8589'
+	, 'Thursday - Friday - Saturday'
+	, 'R/F 4:00 p.m. - 9:00 p.m. S 12:00 p.m. - 9:00 p.m.'
+	, 'Sibling Revelry Brewing was founded by a family of brothers and cousins from Cleveland who wanted to share their passion for great beer with others.
+	 Noticing a lack of breweries in the west side suburbs - the relatives set out to create a unique taproom and drinking experience rooted right in their own backyards. 
+	 Sibling Revelry’s sophisticated taproom offers a full selection of craft beers including year-round brews - seasonal varieties - limited releases and taproom exclusives. 
+	 Our outdoor patio provides additional space to enjoy our brews with friends and dogs.'
+	, 'Warehouse Brewery'
+	, 'true'
+	, 'true'
+	, 'true'
+	, 'true'
+	, 'https://www.siblingrevelrybrewing.com/'
+	, 'https://www.google.com/maps/dir//sibling+revelry+brewing/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x88309204eae51f31:0x2652f1d2afc72563?sa=X&ved=2ahUKEwjjnv2XtaHvAhWTGs0KHVtYDvwQ9RcwE3oECCkQBQ'
+	, 2
+	);
         
 --Beer        
 INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
 VALUES (1, 'Christmas Ale', 7.5, 30, 
-        'Ale with spices and honey', 'A Yuletide`s worth of holiday spices and sweet honey
-        to keep you a-wassailing all season long'
+        'Ale with spices and honey and a Yuletide`s worth of holiday spices and sweet honey to keep you a-wassailing all season long'
         , 1, 'true');
         
 INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active) 
@@ -62,7 +102,117 @@ INSERT INTO beer (beer_id, name, abv,ibu, type, info, brewery_id, is_active)
 VALUES (6, 'Punchline', 5.7, 12, 'Wheat',
         ' Our fruit beer is a tribute to fruit punch. Tons of fruit flavors come together in a haymaker that hits you right in the red rubber nose.'
         , 2, 'true');
+
+--John's inserts
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES (7, 'Rocky River Pirate Light',
+	,  4.8
+	,  16
+	, 'Blonde Ale'
+	, 'This American blonde ale will grow on you. This brew is smooth, golden, and has fewer calories.'
+	,  3
+	, 'true');
+	
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES	(8, 'Hop Goblin IPA'
+	,  7.6
+	,  67.5
+	, 'IPA'
+	, 'This West Coast-style IPA is our new flagship IPA. It has a high level of hop bitterness, flavor and aroma, and just enough malt to balance it.'
+	,  3
+	, 'true');
+	
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES	(9, 'Blarney Rubble Irish Red'
+	,  5.4%
+	,  20.6
+	, 'Irish Red'
+	, 'This is what happened when Bam Bam went to Ireland and had a few too many irish reds and went to visit the Blarney Stone.'
+	,  3
+	, 'true');
+	
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES	(10, 'Cooper’s Gold Kolsch'
+	,  6.0
+	,  28
+	, 'German Ale'
+	, 'This German ale (alt-style) utilizes a unique blend of seven different German malts and two different German hops to produce a golden color.'
+	,  3
+	, 'true');
+	
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES	(11, 'Punchin The Nuts'
+	,  5.3
+	,  20.8
+	, 'Amber Ale'
+	, 'Our amber ale brewed with peanut butter. Which brings out a nice  peanut butter aroma and a light peanut butter taste. It’s everyone’s favorite….everyone love a good “Punchin the Nuts!!!!!!'
+	,  3
+	, 'true');
+	
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES  (12, 'Tropic Funk IPA'
+	,  10.2
+	,  70.2
+	, 'IPA'
+	, 'Citrus is what you smell and taste with a little ipa bitterness. The alcohol is what you will feel.'
+	,  3
+	, 'true');
         
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES (13, 'IPA'
+	,  6.5
+	,  60
+	, 'India Pale Ale'
+	, 'Our Indai Pale Ale has big pine, citrus and stone fruit aromas with a clean, dry finish.  It is like Grandpa Bert - not that bitter once you get to know him.'
+	,  4
+	, 'true');
+
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES 	(14, 'Red'
+	, 5.5
+	, 35
+	, 'American Red Ale'
+	, 'An American red ale brewed in the Irsih tradition.'
+	,  4
+	, 'true');
+	
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES 	(15, 'Swing State'
+	, 4.5
+	, 40
+	, 'Dry Hopped Pale Ale'
+	, 'A dry-hopped, light bodied American Pale Ale.'
+	,  4
+	, 'true');
+	
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES 	(16, 'Coffee Red'
+	,  5.5
+	,  25
+	, 'Coffee Red Ale'
+	, 'We took a few barrels of our Flagship red ale and conditioned it with freshly roasted Rwandan coffee from our friends at Duck-rabbit Coffee. Using whole beans for a softer rounder flavor.'
+	,  4
+	, 'true');
+	
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES (17, 'Siblime'
+	,  5.0
+	,  20
+	, 'Lager brewed with Lime'
+	, 'Light and refereshing, this summer seasonal lager is brewed with just enough lime and spice to remind you that the Cleveland winter is almost over.'
+	,  4
+	, 'true');
+	
+INSERT INTO beer (beer_id, name, abv, ibu, type, info, brewery_id, is_active)
+VALUES (18, 'Mocha Oatmeal Stout'
+	,  6.5
+	,  25
+	, 'Oatmeal Stout'
+	, 'This special batch of our Oatmeal Stout was conditioned with cacao nibs and coffee sourced from Clevelands own Duck-rabbit coffee roasters.'
+	,  4
+	, 'true');
+	
+
 --Reviews
 INSERT INTO reviews (beer_id, user_id, subject_title, review, rating, create_date) 
 VALUES (1, 1, 'Best Christmas present ever!','Who needs a bunch of presents when you only need a six pack of Christmas Ale!',
