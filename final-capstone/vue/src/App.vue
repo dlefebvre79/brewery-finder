69f85d12cc95da6b37b66628af9fc768c111adcf
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2021-03-10 13:10:23 -0700

    gregor modifications to css

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -4,18 +4,15 @@
       <router-link v-bind:to="{ name: 'home' }">Home</router-link> |
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
       <router-link v-bind:to="{ name: 'register'}">Register</router-link>
-      
-     
-      <router-view/>
-      
     </div>
-    
-    <!-- <main>Main</main>
+
+
+    <main class="main"><router-view/></main>
     <div id ="sidebar">Sidebar</div>
     <div id ="content1">Content1</div>
     <div id ="content2">Content2</div>
     <div id = "content3">Content3</div>
-    <footer>Footer</footer> -->
+    <footer>Footer</footer>
    </div>
 </template>
     
