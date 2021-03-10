2f6d54e718a21ba801684b50c9baae16c522e757
Stephen Kuchna <stephedk@gmail.com>
2021-03-10 17:09:52 -0500

    replaced missin | in navigation

--- final-capstone/vue/src/App.vue
+++ final-capstone/vue/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link> |
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>|
       <router-link v-bind:to="{ name: 'register'}">Register</router-link>
     </div>
 
