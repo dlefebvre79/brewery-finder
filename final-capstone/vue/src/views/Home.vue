035f7a7a33fde9cb330eb490b3fbb13aa757a021
Stephen Kuchna <stephedk@gmail.com>
2021-03-15 09:43:03 -0400

    various CSS minor tweaks

--- final-capstone/vue/src/views/Home.vue
+++ final-capstone/vue/src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
 <div class="wrapper">
+  <br>
   <h1 class="home-title">Tech Ale-Evator Brewery Finder</h1>
-  <p class="home-blurb">The world's foremost resource for developer libations</p>
-  <p class="home-blurb">Because if this class didn't make you want to drink, nothing will</p>
+  
+  <p class="home-blurb">The world's foremost resource for developer libations<br><!-- </p> -->
+  <!-- <p class="home-blurb"> -->Because if this class didn't make you want to drink, nothing will</p>
     </div>
    
   
@@ -17,10 +19,16 @@ export default {
 <style>
 .home-title{
   color:darkorange;
+  margin: 0 auto;
+  font-family: 'Noto Sans JP', sans-serif;
+ /*  letter-spacing: 5px; */ 
+  word-spacing: 10px;
+  
   /* text-shadow: 2px 2px 5px; */
 }
 .home-blurb{
   color:darkorange;
+  font-style: italic;
   /* text-shadow: 2px 2px 5px; */
 }
 </style>
