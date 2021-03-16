ea9f2ecf153944d48efdc5ebf7def393c15a2308
John Savage <Egavas6@gmail.com>
2021-03-16 14:53:09 -0400

    NewCSS

--- final-capstone/vue/src/components/ReviewList.vue
+++ final-capstone/vue/src/components/ReviewList.vue
@@ -4,8 +4,8 @@
     <ul>
       <li v-for="review in reviews" v-bind:key="review.id" class="beer-reviews">
         <router-link v-bind:to="{ name: 'beer-information', params: { id: review.beerId } }">
-          {{ review.subjectTitle }}</router-link>
-         {{review.beerName}} | {{ review.review }} | Rating: {{review.rating}} 
+          Rating: {{review.rating}} | {{ review.subjectTitle }} | {{ review.review }} 
+         </router-link> 
       </li>
     </ul>
 
